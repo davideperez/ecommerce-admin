f4f5c19fd8e0b435bddc9c2ebfb1f8a377727d1c
David <david.ezequiel.perez@gmail.com>
2023-09-15 21:11:20 -0300

    enviroment setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
-      <p>Hello Admin Dashboard</p>
-      
-    )
+        <div className="p-4">
+            <Button size='default' variant="destructive" >Click Me</Button>
+        </div>
+      )
 }  
