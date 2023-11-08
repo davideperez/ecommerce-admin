6ac1bb5c9a85d890b6e9d9fa0e27652a7aa4da9c
David <david.ezequiel.perez@gmail.com>
2023-11-08 11:13:52 -0300

    Stripe checkout post implemented.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,20 @@ const nextConfig = {
         domains: [
             "res.cloudinary.com"
         ]
-    }
+    }, // The code below seemed to be necesary but once fixed a name error (a typo on the productIds variable on the axios.post on frontend) and commented, everything still worked fine. But leaving it here just in case.
+/*     async headers() {
+        return [
+            {
+                // matching all API routes:
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*"},
+                    { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+                ]
+            }
+        ]
+    } */
 }
 
 module.exports = nextConfig
