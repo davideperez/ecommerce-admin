392c27325d7394b89258c4453ba603d24a7d17cd
David <david.ezequiel.perez@gmail.com>
2024-08-17 17:50:45 -0300

    Some explanatory comments on the schema.prisma file.

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -9,7 +9,7 @@ import { StoreModal } from "@/components/modals/store-modal";
 export const ModalProvider = () => {
     const [ isMounted, setIsMounted ] = useState(false)
 
-    //To further learn: This logic is to be sure that a hydration error does not ocur.
+    //To further learn: This logic is to be sure that a hydration error does not occur.
     useEffect(()=> {
         setIsMounted(true);
     },[])
